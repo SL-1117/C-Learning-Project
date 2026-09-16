3dc68f264d530d2e531b8171f329ea7a6fa22861
Li wei <liwei20051117@gmail.com>
2026-09-17 00:07:43 +0800

    I build a general idea about the concept but didn't finish it bc I spend too much time trying to understand the vscode and how everything works.

--- Calculator/main.c
+++ Calculator/main.c
@@ -3,7 +3,32 @@
 
 int main() {
 
-    int num1 = 0, num2 = 0, num3 = 0;
+    int user_input1 = 0;
+    int user_input2 = 0;
+    char user_operator = 0;
+
+    printf("Please enter the first integer: \n");
+    scanf("%d", &user_input1);
+
+    printf("Please enter the operator: \n");
+    scanf(" %c", &user_operator);
+
+    printf("Please enter the second integer: \n");
+    scanf("%d", &user_input2);
+
+    if (user_operator == '+') {
+        printf("The result is: %d\n", user_input1 + user_input2);
+    } else if (user_operator == '-') {
+        printf("The result is: %d\n", user_input1 - user_input2);
+    } else if (user_operator == '*') {
+        printf("The result is: %d\n", user_input1 * user_input2);
+    } else if (user_operator == '/') {
+        printf("The result is: %d\n", user_input1 / user_input2);
+    } else if (user_operator == '%') {
+        printf("The result is: %d\n", user_input1 % user_input2);
+    } else {
+        printf("Invalid operator entered.\n");
+    }
 
     return 0;
     
